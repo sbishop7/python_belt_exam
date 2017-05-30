d72ae14372bc4c585cbb73ebdd387b846d61c5a4
sbishop7 <seth.bishop@gmail.com>
2017-05-30 16:41:23 -0700

    updated settings.py with new IP

--- wishlist/wishlist/settings.py
+++ wishlist/wishlist/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ojn^pm51&ri@4w@p#y3mxq%cyb+3kgub-^izk%yel%nbrar=q4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.58.11.108']
 
 
 # Application definition
@@ -109,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
